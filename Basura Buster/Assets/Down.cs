f499d2048cb0ffe1e8d873d69197d966a6510ec1
Alejandro Gutiérrez <cesaragg.2003@mail.com>
2023-11-29 19:54:26 -0600

    caida y movimiento

--- Basura Buster/Assets/Down.cs
+++ Basura Buster/Assets/Down.cs
@@ -2,43 +2,49 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Down : MonoBehaviour
-{
+public class Down : MonoBehaviour {
+    private bool estaSiendoArrastrada = false;
+    private Vector3 posicionInicial;
     public Sprite[] sprites; // Array de sprites para asignar aleatoriamente
     public float tiempoEntreObjetos = 1f; // Tiempo en segundos entre la creación de nuevos objetos
-    public float velocidadCaida = 5f;
-    // Start is called before the first frame update
+    public float velocidadCaida = 1f;
+
     void Start() {
-         // Comienza el ciclo de creación de objetos
+        // Comienza el ciclo de creación de objetos
         InvokeRepeating("CrearNuevoObjeto", 0f, tiempoEntreObjetos);
     }
 
-void CrearNuevoObjeto()
-    {
+    void CrearNuevoObjeto() {
         // Configurar posición inicial
         transform.position = new Vector2(Random.Range(-5f, 5f), 10f);
- 
-        // Asignar un sprite aleatorio si hay al menos un sprite en el arreglo
-        if (sprites.Length > 0)
-        {
-            int indiceSprite = Random.Range(0, sprites.Length);
-            GetComponent<SpriteRenderer>().sprite = sprites[indiceSprite];
-        }
-        else
-        {
-            Debug.LogError("No hay sprites asignados al arreglo. Asigna sprites en el Inspector.");
-        }
+
+        // Asignar un sprite aleatorio
+        int indiceSprite = Random.Range(0, sprites.Length);
+        GetComponent<SpriteRenderer>().sprite = sprites[indiceSprite];
     }
 
-    // Update is called once per frame
-    void Update()
-    {
+    void Update() {
+        // Verificar si el botón del mouse está siendo presionado
+        if (Input.GetMouseButton(0)) {
+            // Si la basura no está siendo arrastrada, registrar la posición inicial
+            if (!estaSiendoArrastrada) {
+                posicionInicial = transform.position;
+                estaSiendoArrastrada = true;
+            }
+
+            // Obtener la posición del mouse en el mundo y actualizar la posición de la basura en el eje X
+            Vector3 posicionMouse = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            transform.position = new Vector3(posicionMouse.x, posicionInicial.y, posicionInicial.z);
+        } else {
+            // Si el botón del mouse no está siendo presionado, la basura ya no está siendo arrastrada
+            estaSiendoArrastrada = false;
+        }
+
         // Mover hacia abajo
         transform.Translate(Vector2.down * velocidadCaida * Time.deltaTime);
 
         // Verificar si el objeto está fuera de la pantalla y destruirlo
-        if (transform.position.y < -20f)
-        {
+        if (transform.position.y < -20f) {
             Destroy(gameObject);
         }
     }
